38b6935670f06b3a7a00ab987f32a4a61989e913
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-11-07 09:42:45 +0100

    ajout des tests

---
+++ tests/Controller/Contact/IndexCest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller\Contact;
+
+use App\Tests\ControllerTester;
+
+class IndexCest
+{
+    public function testAssertThatContactIsGood(ControllerTester $I): void
+    {
+        $I->amOnPage('/contact');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle('Liste des contacts');
+        $I->see('Liste des contacts', 'h1');
+        $I->seeNumberOfElements('li', 195);
+    }
+}
