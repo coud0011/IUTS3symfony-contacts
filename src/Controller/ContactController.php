51cab4017780e9f44ef61495ea209449834a02b5
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-11-07 09:02:47 +0100

    ajout du controller contact et de la route /contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(ContactRepository $repository): Response
+    {
+        $contacts = $repository->findBy([], ['lastname' => 'ASC', 'firstname' => 'ASC']);
+
+        return $this->render('contact/index.html.twig', ['contacts' => $contacts]);
+    }
+}
