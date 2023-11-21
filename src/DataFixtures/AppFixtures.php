f49308842f50b82bf2e02e28b32c9e612b09c276
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-11-21 09:41:08 +0100

    Génération d'une catégorie sans contacts

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
+        CategoryFactory::createOne();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ContactFixtures::class,
+        ];
     }
 }
