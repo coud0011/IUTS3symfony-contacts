eb1b04749f38486a9f40d9ae0993533b3d258a22
Axel COUDROT <axel.coudrot@free.fr>
2023-12-16 13:42:13 +0100

    Configuration du « CRUD » de « User » et Un peu de Style + ajout d'une favicon

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Category;
 use App\Entity\Contact;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,8 +29,15 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
-        yield MenuItem::linkToCrud('Contact', 'fas fa-list', Contact::class);
-        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-tags', Category::class);
+        yield MenuItem::linkToCrud('Contact', 'fas fa-address-book', Contact::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-user-circle', User::class);
+    }
+
+    public function configureAssets(): Assets
+    {
+        $assets = Assets::new();
+
+        return $assets->addCssFile('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined');
     }
 }
